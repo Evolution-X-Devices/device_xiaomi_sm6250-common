a214d92da87b65914f957011144ae5a507e1244e
Alcatraz323 <alcatraz32323@gmail.com>
2025-02-19 23:53:57 +0530

    sm6250-common: Build DSPVolumeSynchronizer Change-Id: I46f37b21fc9f0dd7cfb7f1d72ebd07ae5375bb2f

--- atoll.mk
+++ atoll.mk
@@ -93,6 +93,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm-service.clearkey
 
+# DSPVolumeSynchronizer
+PRODUCT_PACKAGES += \
+    DSPVolumeSynchronizer
+
 # Fastbootd
 PRODUCT_PACKAGES += \
     fastbootd
